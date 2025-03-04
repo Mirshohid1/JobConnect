d22a0e6fc68bdb164a7a49dfaa8f8f936b6da9d6
l0n3r <mirshohid1214@gmail.com>
2025-03-04 18:59:03 +0500

    feat(users): add fields for CustomUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,7 +8,17 @@ def path_to_avatar(instance, filename):
 
 
 class CustomUser(AbstractUser):
-    pass
+    CHOICES_ROLE = [
+        ('user', 'User'),
+        ('admin', 'Admin')
+    ]
+
+    email = models.EmailField(_("email address"), unique=True, null=True, blank=True)
+    avatar = models.ImageField(upload_to=path_to_avatar, null=True, blank=True, verbose_name=_("Avatar"))
+    bio = models.TextField(null=True, blank=True, verbose_name=_("Bio"))
+    role = models.CharField(max_length=10, choices=CHOICES_ROLE, default='user')
+    birth_date = models.DateField(null=True, blank=True)
+    is_email_verified = models.BooleanField(default=False)
 
 
 class SkillType(models.Model):
