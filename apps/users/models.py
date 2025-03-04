4f435e0d422eab01a9e6e9b7d1d3f20219ffb04d
l0n3r <mirshohid1214@gmail.com>
2025-03-04 19:01:38 +0500

    feat(users): add __str__ method for CustomUser

--- apps/users/models.py
+++ apps/users/models.py
@@ -50,6 +50,9 @@ class CustomUser(AbstractUser):
         super().full_clean()
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return f"{self.username}: {self.role}"
+
 
 class SkillType(models.Model):
     pass
