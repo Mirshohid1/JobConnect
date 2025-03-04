111da6eb72b41c4df489acb31cdd8dfdb58bb0e7
l0n3r <mirshohid1214@gmail.com>
2025-03-04 18:57:01 +0500

    feat(users): add structure models

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class SkillType(models.Model):
+    pass
+
+
+class Skill(models.Model):
+    pass
+
+
+class ProfessionType(models.Model):
+    pass
+
+
+class Profession(models.Model):
+    pass
+
+
+class UserSkill(models.Model):
+    pass
+
+
+class UserProfession(models.Model):
+    pass
